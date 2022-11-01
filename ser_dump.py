6020ad84bf5b5da3d854d5f99b26081e62953859
psytrap <44663346+psytrap@users.noreply.github.com>
2022-11-01 09:29:41 +0100

    continuous improvments

--- ser_dump.py
+++ ser_dump.py
@@ -7,21 +7,37 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("serial_device", help="The name of the serial device",
-                    type=str)
+parser = argparse.ArgumentParser()
+parser.add_argument("--hex", help="The mobitec message as hex encode file", type=str)
+parser.add_argument("--port", help="Send message to the serial port", type=str)                    
 args = parser.parse_args()
 
 
-ser = serial.Serial(args.serial_device, 4800, timeout=0.5)
+
+
+ser = serial.Serial(args.port, 4800, timeout=0.5)
+
+if args.hex is not None:
+    with open(args.hex, 'r') as hex_file:
+        for line in hex_file:
+            line = line.rstrip()
+            b = bytes.fromhex(line) #.decode("hex") #bytearray(line, "ascii")
+            print(b.hex())
+            ser.write(b)
+            time.sleep(3)
+    ser.close()
+    exit()
+
+
 
 while True:
     try:
-        time.sleep(0.1)
+        time.sleep(.1)
         bytesToRead = ser.inWaiting()
         if bytesToRead > 0:
             data = ser.read(bytesToRead)
             print(data.hex())
-   except KeyboardInterrupt:
+    except KeyboardInterrupt:
        print('Program Stopped Manually!')
        ser.close()
        raise
